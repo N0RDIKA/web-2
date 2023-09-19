097d21ae5c564a927b833273ef8e98e79e926330
Сердюк Анастасия <serdukasya@gmail.com>
2023-09-19 23:27:25 +0700

    Первое приложение добавление текста

--- app.py
+++ app.py
@@ -3,4 +3,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+       <head>
+<title>Сердюк Анастасия Владиславовна, лабораторная  1</title>
+</head>
+<body>
+<header>
+НГТУ, ФБ, Лаборатоорная работа 1
+</header>
+<h1> web-сервер на flask<h1>
+<footer>
+&copy; Сердюк Анастасия, ФБИ-11Б 3 курс, 2023
+</footer>
+</body>
+</html>
+"""
