c2ed34dafa39a2efeddcccf7aeb22f9ce189a368
Сердюк Анастасия <serdukasya@gmail.com>
2023-10-05 21:37:00 +0700

    Заготовка шаблона

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app. route("/")
@@ -199,3 +199,7 @@ def  mars():
         </body>
 </html>
 '''
+@app. route('/lab2/example')
+def  example(): 
+    return render_template('example.html')
+   
