1f4c70fc3003489cf9a24066da7f30f90e0b500b
Сердюк Анастасия <serdukasya@gmail.com>
2023-10-17 22:23:57 +0700

    Рефакторинг: выделение первой лабораторной

---
+++ lab2.py
@@ -0,0 +1,78 @@
+from flask import Flask, redirect, url_for, render_template
+from lab1 import lab1
+
+
+lab2 = Flask(__name__)
+lab2.register_blueprint(lab1)
+
+
+
+@lab2.route('/lab2/example')
+def  example(): 
+    name = 'Сердюк Анастасия'
+    number = '2'
+    group = 'ФБИ-11'
+    numbercurs = '3'
+    fruits = [
+        {'name':'яблоки', 'price': 100},
+        {'name':'груши', 'price': 120},
+        {'name':'апельсины', 'price': 80},
+        {'name':'мандарины', 'price': 95},
+        {'name':'манго', 'price': 321}
+        ]
+    book = [
+        {'books': 'Тринадцатая сказка', 'str':  420, 'ganr':'Современная зарубежная литература','avtor': 'Диана Сеттерфилд'},
+        {'books': 'Вторая жизнь Уве', 'str':  550, 'ganr':'Современная зарубежная литература','avtor': 'Фредрик Бакман'},
+        {'books': 'Игра престолов', 'str':  875, 'ganr':'Зарубежное фэнтези','avtor': 'Джордж Мартин'},
+        {'books': 'Цветы для Элджернона', 'str':  213, 'ganr':'Научная фантастика','avtor': 'Дэниел Киз'},
+        {'books': 'Не отпускай меня', 'str':  275, 'ganr':'Современная зарубежная литература','avtor': 'Кадзуо Исигуро'},
+        {'books': 'Чтец', 'str':  125, 'ganr':'Современная зарубежная литература','avtor': 'Бернхард Шлинк'},
+        {'books': 'Книжный вор', 'str':  353, 'ganr':'Современная зарубежная литература','avtor': 'Маркус Зусак'},
+        {'books': 'С жизнью наедине', 'str':  403, 'ganr':'Современная зарубежная литература','avtor': 'Кристин Ханна'},
+        {'books': 'Не такая, как все', 'str':  186, 'ganr':'Современная зарубежная литература','avtor': 'Марк Леви'},
+        {'books': 'Соловей', 'str':  369, 'ganr':'Современная зарубежная литература','avtor': 'Кристин Ханна'}
+    ]
+    return render_template('example.html', name=name, number = number, group=group, numbercurs=numbercurs, fruits = fruits, book = book)
+   
+@lab2.route('/lab2/')
+def  lab(): 
+    return render_template('lab2.html')
+
+@lab2.route('/lab2/lab22/')
+def  lab22(): 
+    return render_template('lab2_2.html')
+
+
+
+@lab2.route('/lab2/lab22/flower/')
+def  flower(): 
+    return render_template('lab_flower.html')
+
+
+
+@lab2.route('/lab2/lab22/berry/')
+def  berry(): 
+    return render_template('lab_berry.html')
+
+
+
+@lab2.route('/lab2/lab22/car/')
+def  car(): 
+    return render_template('lab_car.html')
+
+
+
+
+@lab2.route('/lab2/lab22/painting/')
+def  painting(): 
+    return render_template('lab_painting.html')
+
+
+
+
+@lab2.route('/lab2/lab22/furniture/')
+def  furniture(): 
+  return render_template('lab_furniture.html')
+  
+
+
