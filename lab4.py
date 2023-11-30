62d7073e4c3cc145cafe8ad40c4e2fd1d2fd7df8
Serdyuk <Siv0ilovA@gmail.com>
2023-11-30 20:43:01 +0700

    куки

--- lab4.py
+++ lab4.py
@@ -89,4 +89,24 @@ def zerno():
 
 @lab4.route('/lab4/zernoOK')
 def zernoOK():
-    return render_template('zernoOK.html')    
+    return render_template('zernoOK.html') 
+
+
+@lab4.route('/lab4/cookies', methods = ['GET', 'POST'])
+def cookies():
+    if request.method == 'GET':
+        return render_template('cookies.html')
+
+    color = request.form.get('color')
+    b_color = request.form.get('background-color')
+    f_size = request.form.get('font-size')
+    error = ''
+
+
+    headers = {
+        'Set-Cookie':'color=' + color + '; path=/',
+        'Set-Cookie':'font-size=' + f_size + '; path=/',
+        'Set-Cookie':'background-color='  + b_color + '; path=/',
+        'Location': '/lab4/cookies'
+    }
+    return '', 303, headers
