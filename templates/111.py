7ed705f2e7f3a417201a7bc12488f0e46f0a81c6
Сердюк Анастасия <serdukasy@gmail.com>
2024-03-25 16:34:31 +0700

    Создание редми 2

--- templates/111.py
+++
@@ -1,6 +0,0 @@
-data = [[23, "Mikhail", 6.2, 13], [25, "Semyon", 5.9, 0], [29, "Boris", 6.7, 7]] 
-for row in data:
-    if row[3] == 0:
-        print(row)
-    print(row[0] / (row[0] * row[3]))
- 
